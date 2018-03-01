d8f4e22aaa026dc6aec665e40a489ebe92261793
Chris-Chang <hw@yunbin.kr>
2018-03-01 21:00:40 +0900

    add analysis of weekly data
    add analysis of weekly data

--- contributions.py
+++ contributions.py
@@ -1,13 +1,60 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/contribution/<uname>")
-def contribution(uname):
+@app.route("/contributions/<uname>")
+def contributions(uname):
   
   return getContributionsJSON(uname)
+
+@app.route("/contributions/weekly/<uname>")
+def contributionsWeekly(uname):
+  rects = getContributionsElement(uname)
+  sun = 0
+  mon = 0 
+  tue = 0
+  wed = 0
+  thu = 0
+  fri = 0
+  sat = 0
+
+  for rect in rects:
+    dt = rect.get('data-date')
+    count = int(rect.get('data-count'))
+    year, month, day = (int(x) for x in dt.split('-'))
+    ans = datetime.date(year, month, day)
+    dayOfWeek = ans.strftime("%A")
+
+    if dayOfWeek == "Sunday":
+      sun += count
+    elif dayOfWeek == "Monday":
+      mon += count
+    elif dayOfWeek == "Tuesday":
+      tue += count
+    elif dayOfWeek == "Wednesday":
+      wed += count
+    elif dayOfWeek == "Thursday":
+      thu += count
+    elif dayOfWeek == "Friday":
+      fri += count
+    elif dayOfWeek == "Saturday":
+      sat += count
+    
+  json = "{ "\
+            "\"Sunday\" : \"%s\", "\
+            "\"Monday\" : \"%s\", "\
+            "\"Tuesday\" : \"%s\", "\
+            "\"Wednesday\" : \"%s\", "\
+            "\"Thursday\" : \"%s\", "\
+            "\"Friday\" : \"%s\", "\
+            "\"Saturday\" : \"%s\""\
+          " }" % (str(sun), str(mon), str(tue), str(wed), str(thu), str(fri), str(sat))
+
+
+  return json
 
 
 def getContributionsJSON(uname):
