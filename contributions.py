410084dc59f24206deec9d995f0b9353d42306af
Chris-Chang <hw@yunbin.kr>
2018-03-01 18:03:11 +0900

    add flask syntax
    add flask syntax

--- contributions.py
+++ contributions.py
@@ -1,19 +1,27 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/contribution")
+def contribution():
+  url = 'https://github.com/Yunbin-Chang'
+
+  html = urlopen(url)
+  soup = BeautifulSoup(html, 'html.parser')
+
+  rects = soup.find_all("rect")
 
 
-url = 'https://github.com/Yunbin-Chang'
+  json = "["
+  for rect in rects:
+    json += "\n    {\n" + '        "date" : "' + rect.get('data-date') + '"' + " ,\n" + '        "count" : "' + rect.get('data-count') + '"' + "\n    },"
 
-html = urlopen(url)
-soup = BeautifulSoup(html, 'html.parser')
+  json = json[:-1]
+  json += "\n]"
+  print(json)
+  return json
 
-rects = soup.find_all("rect")
-
-
-json = "["
-for rect in rects:
-  json += "\n    {\n" + '        "date" : "' + rect.get('data-date') + '"' + " ,\n" + '        "count" : "' + rect.get('data-count') + '"' + "\n    },"
-
-json = json[:-1]
-json += "\n]"
-print(json)
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
