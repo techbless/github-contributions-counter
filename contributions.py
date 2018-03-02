4fee0190c97ab07d8e198f33e5669a15dd93310a
Chris-Chang <hw@yunbin.kr>
2018-03-02 11:28:27 +0900

    add mothly parser
    add mothly parser

--- contributions.py
+++ contributions.py
@@ -5,7 +5,7 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/contributions/<uname>")
+@app.route("/contributions/daily/<uname>")
 def contributions(uname):
   
   return getContributionsJSON(uname)
@@ -53,6 +53,68 @@ def contributionsWeekly(uname):
             "\"Saturday\" : \"%s\""\
           " }" % (str(sun), str(mon), str(tue), str(wed), str(thu), str(fri), str(sat))
 
+  return json
+
+@app.route('/contributions/monthly/<uname>')
+def contributionsMonthly(uname):
+  rects = getContributionsElement(uname)
+
+  m_jan = 0
+  m_feb = 0
+  m_mar = 0
+  m_apr = 0
+  m_may = 0
+  m_jun = 0
+  m_jul = 0
+  m_aug = 0
+  m_sep = 0
+  m_oct = 0
+  m_nov = 0
+  m_dec = 0
+
+  for rect in rects:
+    dt = rect.get('data-date')
+    count = int(rect.get('data-count'))
+    month = dt.split('-')[1]
+    if month == "01":
+      m_jan += count
+    elif month == "02":
+      m_feb += count
+    elif month == "03":
+      m_mar += count
+    elif month == "04":
+      m_apr += count
+    elif month == "05":
+      m_may += count
+    elif month == "06":
+      m_jun += count
+    elif month == "07":
+      m_jul += count
+    elif month == "08":
+      m_aug += count
+    elif month == "09":
+      m_sep += count
+    elif month == "10":
+      m_oct += count
+    elif month == "11":
+      m_nov += count
+    elif month == "12":
+      m_dec += count
+
+  json = "{ "\
+            "\"January\" : \"%s\", "\
+            "\"Febuary\" : \"%s\", "\
+            "\"March\" : \"%s\", "\
+            "\"April\" : \"%s\", "\
+            "\"May\" : \"%s\", "\
+            "\"June\" : \"%s\", "\
+            "\"July\" : \"%s\","\
+            "\"August\" : \"%s\","\
+            "\"September\" : \"%s\","\
+            "\"October\" : \"%s\","\
+            "\"November\" : \"%s\","\
+            "\"December\" : \"%s\","\
+          " }" % (str(m_jan), str(m_feb), str(m_mar), str(m_apr), str(m_may), str(m_jun), str(m_jul), str(m_aug), str(m_sep), str(m_oct), str(m_nov), str(m_dec))
 
   return json
 
