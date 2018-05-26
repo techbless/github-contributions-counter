87f968053592e86f04a8a7c62343acd86487472a
Chris-Chang <hw@yunbin.kr>
2018-05-26 22:48:27 +0900

    add test file for testing

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+# In the same directory, you can import here like the just below did.
+
 from contributions import *
+
+
+# This is only for identifying if API is working or Testing Something bizarre.
 
 print(getContributionsMonthly("Yunbin-Chang\n\n"))
 print(getContributionsWeekly("Yunbin-Chang\n\n"))
