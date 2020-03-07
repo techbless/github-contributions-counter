b390e383e1d8d77c6c8ad854f8d6078050bffc00
Yunbin Chang <yunbin@hansung.ac.kr>
2020-03-07 05:32:50 +0000

    Remove cache storage.

--- back-end/contributions.py
+++ back-end/contributions.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from cache import CacheStorage
 import requests
 import datetime
 import json
@@ -14,6 +13,7 @@ import time
 
 # MIT License
 # Copyright (c) 2018 Chris Yunbin Chang
+
 class Contribution():
   monthDict = {
     1: "January",
@@ -40,156 +40,118 @@ class Contribution():
     6: "Saturday"
   }
 
-  def __init__(self, *backup_file_name):
-    if backup_file_name:
-      self.cache = CacheStorage(backup_file_name[0])
-    else:
-      self.cache = CacheStorage()
+  def getContributionsDaily(self, uname):  
+    res_list = []
+    rects = self.getContributionsElement(uname)
 
-  def close(self):
-    del self.cache
-
-
-  def getContributionsDaily(self, uname):
+    if rects == "404":
+      return "USER NOT FOUND"
     
-    if self.cache.checkCacheExistence("day", uname):
-      result = self.cache.getCache("day", uname)
-    else:
-      res_list = []
-      rects = self.getContributionsElement(uname)
+    for rect in rects:
+      if rect.get('data-count') != "0":
+        dic = {
+          "date": rect.get('data-date'),
+          "count": rect.get('data-count')
+        }
+        res_list.append(dic)
 
-      if rects == "404":
-        return "USER NOT FOUND"
-      
-      for rect in rects:
-        if rect.get('data-count') != "0":
-          dic = {
-            "date": rect.get('data-date'),
-            "count": rect.get('data-count')
-          }
-          res_list.append(dic)
-
-      result = json.dumps(res_list)
-      self.cache.createCache("day", uname, result)
-
-    return result
+    return json.dumps(res_list)
 
   def getContributionsWeekly(self, uname):
+    rects = self.getContributionsElement(uname)
 
-    if self.cache.checkCacheExistence("week", uname):
-      result = self.cache.getCache("week", uname)
-    else:
-      rects = self.getContributionsElement(uname)
+    if rects == "404":
+      return "USER NOT FOUND"
 
-      if rects == "404":
-        return "USER NOT FOUND"
+    week = [0, 0, 0, 0, 0, 0, 0]
 
-      week = [0, 0, 0, 0, 0, 0, 0]
+    for rect in rects:
+      dt = rect.get('data-date')
+      count = int(rect.get('data-count'))
+      year, month, day = (int(x) for x in dt.split('-'))
+      ans = datetime.date(year, month, day)
+      dayOfWeek = ans.strftime("%A")
 
-      for rect in rects:
-        dt = rect.get('data-date')
-        count = int(rect.get('data-count'))
-        year, month, day = (int(x) for x in dt.split('-'))
-        ans = datetime.date(year, month, day)
-        dayOfWeek = ans.strftime("%A")
-
-        if dayOfWeek == "Sunday":
-          week[0] += count
-        elif dayOfWeek == "Monday":
-          week[1] += count
-        elif dayOfWeek == "Tuesday":
-          week[2] += count
-        elif dayOfWeek == "Wednesday":
-          week[3] += count
-        elif dayOfWeek == "Thursday":
-          week[4] += count
-        elif dayOfWeek == "Friday":
-          week[5] += count
-        elif dayOfWeek == "Saturday":
-          week[6] += count
+      if dayOfWeek == "Sunday":
+        week[0] += count
+      elif dayOfWeek == "Monday":
+        week[1] += count
+      elif dayOfWeek == "Tuesday":
+        week[2] += count
+      elif dayOfWeek == "Wednesday":
+        week[3] += count
+      elif dayOfWeek == "Thursday":
+        week[4] += count
+      elif dayOfWeek == "Friday":
+        week[5] += count
+      elif dayOfWeek == "Saturday":
+        week[6] += count
         
-      res = []
-
-      d = 0
-      for day_count in week:
-        dic = {
-          "day_of_week": self.weekTable[d],
-          "count": day_count
-        }
-        res.append(dic)
-        d += 1
+    res = []
+    d = 0
+    for day_count in week:
+      dic = {
+        "day_of_week": self.weekTable[d],
+        "count": day_count
+      }
+      res.append(dic)
+      d += 1
     
-      result = json.dumps(res)
-      self.cache.createCache("week", uname, result)
-
-    return result
-
+    return json.dumps(res)
 
   def getContributionsMonthly(self, uname):
-    if self.cache.checkCacheExistence("month", uname):
-      result = self.cache.getCache("month", uname)
-    else:
-      rects = self.getContributionsElement(uname)
+    rects = self.getContributionsElement(uname)
 
-      if rects == "404":
-        return "USER NOT FOUND"
+    if rects == "404":
+      return "USER NOT FOUND"
 
-      monthOf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    monthOf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-      for rect in rects:
-        dt = rect.get('data-date')
-        count = int(rect.get('data-count'))
-        month = int(dt.split('-')[1])
+    for rect in rects:
+      dt = rect.get('data-date')
+      count = int(rect.get('data-count'))
+      month = int(dt.split('-')[1])
 
-        for n in range(1, 13):
-          if month == n:
-            monthOf[n] += count
+      for n in range(1, 13):
+        if month == n:
+          monthOf[n] += count
 
-      monthDict = {
-        1: "January",
-        2: "Febuary",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
-      }
+    monthDict = {
+      1: "January",
+      2: "Febuary",
+      3: "March",
+      4: "April",
+      5: "May",
+      6: "June",
+      7: "July",
+      8: "August",
+      9: "September",
+      10: "October",
+      11: "November",
+      12: "December"
+    }
 
-      res = []
+    res = []
+    m = 0
+    for month in monthOf:
+      if m != 0:
+        dic = {
+          "month": monthDict[m],
+          "count": str(month)
+        }
+        res.append(dic)
+      m += 1
 
-      m = 0
-      for month in monthOf:
-        if m != 0:
-          dic = {
-            "month": monthDict[m],
-            "count": str(month)
-          }
-          res.append(dic)
-        m += 1
-
-      result = json.dumps(res)
-      self.cache.createCache("month", uname, result)
-    return result
+    return json.dumps(res)
 
   def getContributionsRatio(self, uname):
     try:
-      if self.cache.checkCacheExistence("ratio", uname):
-        result = self.cache.getCache("ratio", uname)
-      else:
-        url = 'https://github.com/' + uname
-        #html = urlopen(url)
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'lxml')
+      url = 'https://github.com/' + uname
+      html = requests.get(url).text
+      soup = BeautifulSoup(html, 'lxml')
 
-        ov_graph = soup.find("div", {'class': 'js-activity-overview-graph-container'})
-        result = ov_graph.get('data-percentages').replace(' ', '_').lower()  # already json
-        self.cache.createCache("ratio", uname, result)
-      return result
+      ov_graph = soup.find("div", {'class': 'js-activity-overview-graph-container'})
+      return ov_graph.get('data-percentages').replace(' ', '_').lower()  # already json
     except:
       return "NOT PUBLIC DATA"
 
